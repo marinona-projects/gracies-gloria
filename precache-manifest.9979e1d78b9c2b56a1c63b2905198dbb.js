6731515a6bbede24d4870dff3b1ca6fb29852a2e
marinona-tests <marinagaving@gmail.com>
2020-06-15 16:47:59 +0200

    Updates

---
+++ precache-manifest.9979e1d78b9c2b56a1c63b2905198dbb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25b64eb604e16224f1d0095847f13a0",
+    "url": "/gracies-gloria/index.html"
+  },
+  {
+    "revision": "82d06a5fd7039c6523b1",
+    "url": "/gracies-gloria/static/css/2.dafc00e9.chunk.css"
+  },
+  {
+    "revision": "b8f8883b08d2e5f19e5f",
+    "url": "/gracies-gloria/static/css/main.ecc0754c.chunk.css"
+  },
+  {
+    "revision": "82d06a5fd7039c6523b1",
+    "url": "/gracies-gloria/static/js/2.5ab43e7b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gracies-gloria/static/js/2.5ab43e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f8883b08d2e5f19e5f",
+    "url": "/gracies-gloria/static/js/main.2eedba97.chunk.js"
+  },
+  {
+    "revision": "815c7b9d582ab99a1408",
+    "url": "/gracies-gloria/static/js/runtime-main.049cfd83.js"
+  },
+  {
+    "revision": "a266e785c6171c9968c748078bffb353",
+    "url": "/gracies-gloria/static/media/01.a266e785.png"
+  },
+  {
+    "revision": "3a9ab7b74ef7e38ce87935cd1949b7bf",
+    "url": "/gracies-gloria/static/media/02.3a9ab7b7.png"
+  },
+  {
+    "revision": "93f415a81753522f25d9bc54afd6bf1f",
+    "url": "/gracies-gloria/static/media/03.93f415a8.png"
+  },
+  {
+    "revision": "64c5d6aa3335ba49ff4cba47273d5656",
+    "url": "/gracies-gloria/static/media/04.64c5d6aa.png"
+  },
+  {
+    "revision": "fb3ab8b4b9d0deaa1e027a5ca6a7e5be",
+    "url": "/gracies-gloria/static/media/05.fb3ab8b4.png"
+  },
+  {
+    "revision": "914ea944ec2adc32cbf4bc9196a0ca19",
+    "url": "/gracies-gloria/static/media/06.914ea944.png"
+  },
+  {
+    "revision": "59b697db51622c6bc30ae1eca5f49ced",
+    "url": "/gracies-gloria/static/media/background.59b697db.png"
+  }
+]);
