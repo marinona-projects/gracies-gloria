af6008c0c3634328ee34feb4de9a22f628d9a1ce
marinona-tests <marinagaving@gmail.com>
2020-06-19 21:13:33 +0200

    Updates

--- precache-manifest.ea799ebca95c3fb35856eea942f7c6c8.js
+++ precache-manifest.ea799ebca95c3fb35856eea942f7c6c8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ab7a0d90d903fabf411b3be0edac29a1",
+    "revision": "7d0498bba31e59e492c612dd98464b04",
     "url": "/gracies-gloria/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gracies-gloria/static/css/2.dafc00e9.chunk.css"
   },
   {
-    "revision": "ae12d1dcfa0a27f08833",
+    "revision": "108c170d6549437e411e",
     "url": "/gracies-gloria/static/css/main.d954f2a5.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gracies-gloria/static/js/2.734df239.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ae12d1dcfa0a27f08833",
-    "url": "/gracies-gloria/static/js/main.2ae3c3b1.chunk.js"
+    "revision": "108c170d6549437e411e",
+    "url": "/gracies-gloria/static/js/main.41553887.chunk.js"
   },
   {
     "revision": "815c7b9d582ab99a1408",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gracies-gloria/precache-manifest.2075f7e7e6fd6dc753a8597ab653e47f.js"
+  "/gracies-gloria/precache-manifest.ea799ebca95c3fb35856eea942f7c6c8.js"
 );
 
 self.addEventListener('message', (event) => {
